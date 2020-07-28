0b41b6df33fe8c8131c0717d054220d8105a66cb
Alexander Horch <alexander.horch@mathema.de>
2020-07-28 08:08:13 +0200

    2015 day 25

--- 2015/Day25/solution.py
+++ 2015/Day25/solution.py
@@ -3,11 +3,13 @@ from puzzle_input import row, column, first, factor, modulo
 current = first
 
 diagonal = row + column - 1
-n = sum(range(diagonal+1)) + column-1
+n = sum(range(diagonal)) + column - 1
+
 
 def next():
     global current
     current = (current * factor) % modulo
+
 
 for i in range(n):
     next()
