933d8c291defa72bbcdc8f66b0432564a5fdbda8
Alexander Horch <alexander@Alexanders-MacBook-Air.local>
2020-08-11 13:25:52 +0200

    2015 18

--- 2015/Day18/solution.py
+++ 2015/Day18/solution.py
@@ -7,25 +7,32 @@ with open("input.txt", "r") as file:
 
 
 def state(field, i, j):
+    size = len(field)
     return 0 if -1 in [i, j] or len(field) in [i, j] else field[i][j]
 
 
 def next_state(field, i, j):
+    last = len(field)-1
     tot = 0
     for x in [-1, 0, 1]:
         for y in [-1, 0, 1]:
             if x == y == 0:
                 continue
-            tot += state(field, i, j)
-    return [0, 1][tot == 3 or (field[i][j] and tot == 2)]
+            tot += state(field, i+x, j+y)
+    return [0, 1][tot == 3 or (field[i][j] and tot == 2) or (i in [0, last] and j in [0, last])]
 
 
 def step(field):
     size = len(field)
     field2 = []
     for i in range(size):
-        field2.append([0]*size)
-    for i in range(size):
+        field2.append([])
         for j in range(size):
-            field2[i][j] = next_state(field, i, j)
+            field2[i].append(next_state(field, i, j))
     return field2
+
+
+p = field
+for i in range(100):
+    p = step(p)
+print(sum(map(sum, p)))
--- 2015/Day18/test_solution.py
+++ 2015/Day18/test_solution.py
@@ -1,22 +1,63 @@
 import unittest
 import solution
 
-example = """.#.#.#
+examples = [""".#.#.#
 ...##.
 #....#
 ..#...
 #.#..#
-####.."""
+####..""", """..##..
+..##.#
+...##.
+......
+#.....
+#.##..""", """..###.
+......
+..###.
+......
+.#....
+.#....""", """...#..
+......
+...#..
+..##..
+......
+......""", """......
+......
+..##..
+..##..
+......
+......"""]
 
 
 class TestSolution(unittest.TestCase):
     def test_parse(self):
         self.assertListEqual([[0, 1, 0, 1, 0, 1], [0, 0, 0, 1, 1, 0], [1, 0, 0, 0, 0, 1], [
-                             0, 0, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1], [1, 1, 1, 1, 0, 0]], solution.parse_input(example))
+                             0, 0, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1], [1, 1, 1, 1, 0, 0]], solution.parse_input(examples[0]))
 
-    def test_step(self):
-        self.assertListEqual([[0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 0, 1], [0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0], [
-                             1, 0, 0, 0, 0, 0], [1, 0, 1, 1, 0, 0]], solution.step(solution.parse_input(example)))
+    def test_state(self):
+        f = solution.parse_input(examples[0])
+        self.assertEqual(0, solution.state(f, 0, 0))
+        self.assertEqual(0, solution.state(f, -1, 0))
+        self.assertEqual(0, solution.state(f, 0, -1))
+        self.assertEqual(0, solution.state(f, 6, 0))
+        self.assertEqual(0, solution.state(f, 0, 6))
+        self.assertEqual(1, solution.state(f, 0, 1))
+
+    def test_step_1(self):
+        self.assertListEqual(solution.parse_input(
+            examples[1]), solution.step(solution.parse_input(examples[0])))
+
+    def test_step_2(self):
+        self.assertListEqual(solution.parse_input(
+            examples[2]), solution.step(solution.parse_input(examples[1])))
+
+    def test_step_3(self):
+        self.assertListEqual(solution.parse_input(
+            examples[3]), solution.step(solution.parse_input(examples[2])))
+
+    def test_step_4(self):
+        self.assertListEqual(solution.parse_input(
+            examples[4]), solution.step(solution.parse_input(examples[3])))
 
 
 if __name__ == '__main__':
