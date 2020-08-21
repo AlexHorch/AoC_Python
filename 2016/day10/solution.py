4604adc95fee933be3de98b7ac269898945d79ca
Alexander Horch <malexhorch@gmail.com>
2020-08-21 09:51:56 +0200

    2016 10 Zwischenstand

---
+++ 2016/day10/solution.py
@@ -0,0 +1,11 @@
+
+with open("input.txt", "r") as file:
+    instructions = file.read().split("\n")
+
+
+class Bot:
+    def addRecipients(self, low, high):
+        pass
+
+
+bot = {}
---
+++ 2016/day10/test_solution.py
@@ -0,0 +1,11 @@
+import unittest
+from solution import combinations
+
+
+class TestSolution(unittest.TestCase):
+    def test_bot(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
