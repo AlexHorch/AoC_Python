ce519d059afbacfcb53052f06472ff4078b42c9e
Alexander Horch <malexhorch@gmail.com>
2020-08-26 09:43:46 +0200

    2018 4

--- 2018/day04/solution.rb
+++ 2018/day04/solution.rb
@@ -3,14 +3,32 @@ require 'date'
 #[1518-07-08 00:02] falls asleep
 #[1518-05-22 23:50] Guard #797 begins shift
 
-input = File.open("input.txt").read
+
+
+
+input = File.open("input.txt").read.split"\n"
+f = File.open("output.txt", "w")
+input.sort_by! {|line| DateTime.parse(line.split(/[\[\]]/)[1])}.each do |line| f.write line + "\n" end
 
 class Entry
     def initialize(text)
-        @timestamp = DateTime(text.split(/[\[\]]/)[1])
-        @msg = text.split("]")[1]
+        @timestamp = DateTime.parse(text.split(/[\[\]]/)[1])
+        @msg = text.split("] ")[1]
     end
 
-    attraccessor :timestamp
-    attraccessor :msg
+    attr_accessor :timestamp
+    attr_accessor :msg
+end
+
+class Guard
+    def initialize(id)
+        @id = id
+        @sleepcycle = Array.new(60, 0)
+        @shifts = Hash.new
+    end
+
+    def start_shift(month, day)    end
+
+
+    attr_accessor :id
 end
